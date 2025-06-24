78fd5b055b8a28f75099eda9bb7161accf334449
bikejojo <87353248+bikejojo@users.noreply.github.com>
2025-06-23 23:55:50 -0400

    funciones

---
+++ src/models/Evento.js
@@ -0,0 +1,8 @@
+class Evento {
+    constructor(nombreEvento,descripcionSala,tiempoInicio,tiempoFin){
+        this.nombreEvento = nombreEvento
+        this.descripcionSala = descripcionSala;
+        this.tiempoInicio = tiempoInicio;
+        this.tiempoFin = tiempoFin;
+    }
+}
---
+++ src/utils/validaciones.js
@@ -0,0 +1,33 @@
+class Validaciones {
+
+    /**
+    * @param {Array} data - Lista de eventos
+    */
+    static validacionSala(data){
+        if(data.length > 0){
+             for (let i = 0; i < data.length; i++) {
+                for (let j = i + 1; j < data.length; j++) {
+                    if (data[i].descripcionSala === data[j].descripcionSala) {
+                       const tiempoValido = this.validacionTiempo(data[i], data[j]);
+                        if (!tiempoValido.valid) {
+                            return tiempoValido; // Si hay conflicto, retornamos
+                        }
+                    }
+                }
+            }
+        }
+        return { valid: true, message: 'No hay conflictos de horarios en las salas.' };
+    }
+
+    static validacionTiempo(event1 , event2){
+         if(event1.tiempoInicio < event2.tiempoFin && event2.tiempoInicio < event1.tiempoFin ){
+            return {
+                valid:false,
+                message:`Conflicto de eventos ${event1.nombreEvento} con ${event2.nombreEvento}`
+            };
+        }
+        return { valid: true };
+    }
+}
+
+module.exports  = Validaciones;
